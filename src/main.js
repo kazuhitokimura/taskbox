e720fa070c6f6ddbd6a3fbb46f9a70d273f2470c
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-06-18 15:11:14 +0900

    pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
 
 import './assets/main.css'
 
+app.use(pinia)
 createApp(App).mount('#app')
